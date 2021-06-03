318f0e9b7579f146a21a47f3f80574e701b90d96
Dominik Korosec <dominik.korosec@inova.si>
2021-06-03 16:44:33 +0200

    Major release: * changed structure of dumped logging object (key.data.log/err -> key.log/err) ~ accessing the children via path is still the same (key['/'].key2)
    * added trace method, that traces log calls via stack trace that Error class provides.

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const sharedLogger = require('./lib/shared-logger')
-
+const _tracePrivateSym = Symbol('_tracePrivateMethodKey')
 function createDiagnosticsEntry (stateNode) {
   const stateNodeData = sharedLogger.getStateNodeData(stateNode)
+
   return {
+    [_tracePrivateSym] (message, stackTrace) {
+      stateNodeData.trace = {
+        payload: message,
+        stackTrace,
+        ts: new Date().toISOString()
+      }
+    },
     path (path) {
       const nextStateNode = sharedLogger._createKeyPathStateNode(path, stateNode)
       return createDiagnosticsEntry(nextStateNode)
     },
     log (message) {
       stateNodeData.log = {
-        message,
+        payload: message,
         ts: new Date().toISOString()
       }
     },
     error (message, error) {
       stateNodeData.error = {
-        message,
+        payload: message,
         error,
         ts: new Date().toISOString()
       }
+    },
+    trace (message) {
+      const err = new Error('trace')
+      // stack index: 0 = Error, 1 = self, 2 is the callee (our interest), 3 and beyond the call-stack that came to us.
+      const actualStack = err.stack.split('\n').filter((_, i) => i > 1).map(txt => txt.trim())
+      const [traceEntry] = actualStack
+      const path = traceEntry.match(/\((.*)\)/)[1].replace(/\\/g, '>')
+      this.path('__trace__').path(path)[_tracePrivateSym](message, actualStack)
     },
     dump ({ includeChildren = false } = {}) {
       if (includeChildren) {
